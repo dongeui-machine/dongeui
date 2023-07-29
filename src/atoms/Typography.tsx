0f6513e17f52a58d14cdadf4e67cbd97dc1f4574
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-07-29 17:46:54 +0900

    Button, Typography 아토믹 디자인 작성

--- src/atoms/Typography.tsx
+++ src/atoms/Typography.tsx
@@ -1,31 +1,38 @@
 import token from '../tokens/tokens.json';
 import { styled } from 'styled-components';
 
+const { Heading1, Heading2, Heading3, bodyText, smallText } = token.global;
+
 export const Heading1Typo = styled.h1`
+    font-family: 'Noto Sans KR', sans-serif;
     margin: 0;
-    font-size: 40px;
+    font-size: ${Heading1.value}px;
 `;
 export const Heading2Typo = styled.h2`
+    font-family: 'Noto Sans KR', sans-serif;
     margin: 0;
-    font-size: 36px;
+    font-size: ${Heading2.value}px;
 `
 export const Heading3Typo = styled.h3`
+    font-family: 'Noto Sans KR', sans-serif;
     margin: 0;
-    font-size: 32px;
+    font-size: ${Heading3.value}px;
 `
 export const BodyTextTypo = styled.div`
+    font-family: 'Noto Sans KR', sans-serif;
     margin: 0;
-    font-size: 24px;
+    font-size: ${bodyText.value}px;
 `
 export const SmallTextTypo = styled.span`
+    font-family: 'Noto Sans KR', sans-serif;    
     margin: 0;
-    font-size: 20px;
+    font-size: ${smallText.value}px;
 `
-export type TitleProps = {
+export type TypoProps = {
     text?: string;
 }
 
-export const Typography = ({ text } : TitleProps) => {
+export const Typography = ({ text } : TypoProps) => {
     return (<>
         <Heading1Typo>{ text }</Heading1Typo>
         <Heading2Typo>{ text }</Heading2Typo>
