dd96181a3ea0d51fadefda31503f8185e6fc8b9a
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-08-02 17:27:56 +0900

    npm install 안되는거 해결

--- src/tokens/GlobalStyled.tsx
+++ src/tokens/GlobalStyled.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import { normalize } from "styled-normalize";
 
 const GlobalStyles = createGlobalStyle`
-    ${normalize};
     * {
         box-sizing: border-box;
         font-family: 'Noto Sans KR', sans-serif;
